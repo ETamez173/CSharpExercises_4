c4b557c72909931cbc4f4f9186fb04aeb3ece95e
ETamez173 <56414835+ETamez173@users.noreply.github.com>
2021-03-08 16:08:01 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+// Variables | C# | Tutorial 4
+namespace CSharpExercises_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // create variable and the type of data to store values 
+            
+
+            // this declares a variable and gives it a value
+            string characterName = "John";
+
+            // this declares a variable
+            int characterAge;
+            // then we assign a value to the variable
+            characterAge = 70;
+
+
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge  + " years old");
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But he didn't like being " + characterAge);
+
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
